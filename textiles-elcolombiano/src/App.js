8dc151f451460e1a08cad0873e3768b195092920
ldlasso12 <laurad.lasso.a@gmail.com>
2021-10-11 17:04:09 -0500

    Componentes Navbar y Menu, Pagina principal para colocar componentes con routes

--- textiles-elcolombiano/src/App.js
+++ textiles-elcolombiano/src/App.js
@@ -1,21 +1,26 @@
 import React from 'react'
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Ventanas from './pages/Ventanas';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <br />
+        <Switch>
+            <Route path="/ventas">
+              <Ventanas objeto="ventas" />
+            </Route>
+            <Router path="/productos">
+              <Ventanas objeto="productos" />
+            </Router>
+            <Router path="/usuarios">
+              <Ventanas objeto="usuarios" />
+            </Router>
+        </Switch>
+      </Router>  
     </div>
   );
 }
